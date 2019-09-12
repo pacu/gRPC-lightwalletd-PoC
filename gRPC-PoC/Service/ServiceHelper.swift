5f691e73faa79d9ed51683bd67f4b5d4f62bcffc
Francisco Gindre <francisco.gindre@gmail.com>
2019-09-12 10:19:08 -0300

    Streams PoC Tests (#2)
    * Improve tests
    
    * fix stream tests
    
    * Streams tests

--- gRPC-PoC/Service/ServiceHelper.swift
+++ gRPC-PoC/Service/ServiceHelper.swift
@@ -10,26 +10,91 @@ import Foundation
 import SwiftGRPC
 
 class Environment {
+    
     static let lightwalletdKey = "LIGHTWALLETD_ADDRESS"
     
     static var address: String {
         return Constants.address
     }
+    
 }
+
 class ServiceHelper {
+    
     static let shared = ServiceHelper()
     
     let compactTxStreamer: CompactTxStreamerServiceClient
+    
     private init() {
        compactTxStreamer = CompactTxStreamerServiceClient(address: Environment.address, secure: false, arguments:[])
-        
     }
+    
+    func getBlockRange(startHeight: UInt64, endHeight: UInt64? = nil,  result: @escaping (CallResult)->()) throws -> CompactTxStreamerGetBlockRangeCall {
+           try compactTxStreamer.getBlockRange(BlockRange(startHeight: startHeight, endHeight: endHeight)) { result($0) }
+    }
+    
+    
     
 }
 
 extension ServiceHelper {
+    
     func latestBlock() throws -> BlockID {
         try compactTxStreamer.getLatestBlock(ChainSpec())
     }
+    
+    func getTx(hash:String) throws -> RawTransaction {
+        var filter = TxFilter()
+        filter.hash = Data(hash.utf8)
+        return try compactTxStreamer.getTransaction(filter)
+    }
+    
+    func getAllBlocksSinceSaplingLaunch(_ result: @escaping (CallResult)->()) throws -> CompactTxStreamerGetBlockRangeCall {
+        try compactTxStreamer.getBlockRange(BlockRange.sinceSaplingActivation(), completion: result)
+    }
 }
 
+
+extension Range where Element == UInt64 {
+    func blockRange() -> BlockRange {
+        BlockRange(startHeight: lowerBound, endHeight: upperBound)
+    }
+}
+
+
+extension BlockID {
+    
+    static let saplingActivationHeight: UInt64 = 280_000
+    
+    init(height: UInt64) {
+        self = BlockID()
+        self.height = height
+    }
+    
+    static var saplingActivation: BlockID {
+        BlockID(height: saplingActivationHeight)
+    }
+    
+}
+
+extension BlockRange {
+    
+    init(startHeight: UInt64, endHeight: UInt64? = nil) {
+        self = BlockRange()
+        self.start =  BlockID(height: startHeight)
+        if let endHeight = endHeight {
+            self.end = BlockID(height: endHeight)
+        }
+    }
+    
+    static func sinceSaplingActivation(to height: UInt64? = nil) -> BlockRange {
+       var blockRange = BlockRange()
+        
+        blockRange.start = BlockID.saplingActivation
+        if let height = height {
+            blockRange.end = BlockID.init(height: height)
+        }
+        return blockRange
+    }
+    
+}
--- gRPC-PoCTests/gRPC_PoCTests.swift
+++ gRPC-PoCTests/gRPC_PoCTests.swift
@@ -10,7 +10,8 @@ import XCTest
 @testable import gRPC_PoC
 
 class gRPC_PoCTests: XCTestCase {
-
+    
+    static var latestBlock: BlockID = try! ServiceHelper.shared.latestBlock()
 
     func testEnvironmentLaunch() {
         
@@ -20,14 +21,48 @@ class gRPC_PoCTests: XCTestCase {
     }
     
     func testService() {
-        
-        let latestBlock = try? ServiceHelper.shared.latestBlock()
-        
-        // Check that your block has been retrieved
-        XCTAssertNotNil(latestBlock)
-        
+   
         // and that it has a non-zero size
-        XCTAssert(latestBlock!.height > 0)
+        XCTAssert(Self.latestBlock.height > 0)
         
     }
+    
+    func testBlockRangeService() {
+
+        let expect = XCTestExpectation(description: self.debugDescription)
+        let _ = try? ServiceHelper.shared.getAllBlocksSinceSaplingLaunch(){ result in
+            print(result)
+            expect.fulfill()
+            XCTAssert(result.success)
+            XCTAssertNotNil(result.resultData)
+        }
+        wait(for: [expect], timeout: 10)
+    }
+    
+    func testBlockRangeServiceTilLastest() {
+        let expectedCount: UInt64 = 99
+        var count: UInt64 = 0
+        let expect = XCTestExpectation(description: self.debugDescription)
+        
+        let startHeight = Self.latestBlock.height - expectedCount
+        let endHeight = Self.latestBlock.height
+        guard let call = try? ServiceHelper.shared.getBlockRange(startHeight: startHeight, endHeight: endHeight,result: {
+            result in
+                       XCTAssert(result.success)
+                   
+        }) else {
+            XCTFail("failed to create getBlockRange( \(startHeight) ..<= \(endHeight)")
+            return
+        }
+        wait(for: [expect], timeout: 20)
+        
+        while let _ = try? call.receive() {
+            expect.fulfill()
+            count += 1
+        }
+        
+        XCTAssertEqual(expectedCount + 1, count)
+        
+    }
+    
 }
